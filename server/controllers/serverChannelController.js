71e56638002937572f5c6c47d8bf40e2c3a1e585
Tyler Hawkins <thawkinsusa>
2019-08-19 12:09:26 -0600

    server reducer fix

--- server/controllers/serverChannelController.js
+++ server/controllers/serverChannelController.js
@@ -1,44 +1,43 @@
 module.exports = {
-    createServerChannel: async (req, res, next) => {
+    createServer: async (req, res, next) => {
       const dbInstance = req.app.get('db');
       const { server_id, server_name, server_image, user_id } = req.body
       console.log(req.body);
-      const serverChannelId = await dbInstance.create_server_channel([server_id, server_name, server_image, user_id])
+      const serverChannelId = await dbInstance.create_server([server_id, server_name, server_image, user_id])
       await dbInstance.create_server_users_junction(server_id, user_id)
-      const data = await dbInstance.get_server_channel_by_user_id(user_id)
+      const data = await dbInstance.get_server_by_user_id(user_id)
       res.status(200).send(data)
   
     },
-    getServerChannel: (req, res, next) => {
+    getServer: (req, res, next) => {
       const dbInstance = req.app.get('db');
-      console.log('hit get team');
-      dbInstance.get_server_channel_by_user_id(req.params.id)
+      dbInstance.get_server_by_user_id(req.params.id)
         .then(server => {
           res.status(200).send(server)
         })
         .catch(err => {
-          res.status(500).send({ errorMessage: "Something went wrong with get Channel by channel Id" });
+          res.status(500).send({ errorMessage: "Something went wrong with get server by user Id" });
           console.log(err)
         });
     },
   
-    getServerChannelMembers: async (req, res, next) => {
+    getServerUsers: async (req, res, next) => {
       const dbInstance = req.app.get('db');
       const { user_id } = req.params
-      dbInstance.get_server_channel_members(req.params.id)
-        .then(members => {
-          res.status(200).send(members)
+      dbInstance.get_server_users(req.params.id)
+        .then(users => {
+          res.status(200).send(users)
         })
         .catch(err => {
-          res.status(500).send({ errorMessage: "get members is broken" });
+          res.status(500).send({ errorMessage: "get users is broken" });
           console.log(err)
         });
     },
   
-    getAllServers: (req, res, next) => {
+    getServers: (req, res, next) => {
       const dbInstance = req.app.get('db');
   
-      dbInstance.get_server_channels()
+      dbInstance.get_servers()
         .then(servers => res.status(200).send(servers))
         .catch(err => {
           res.status(500).send({ errorMessage: "getAllServers is broken" });
@@ -46,25 +45,25 @@ module.exports = {
         });
     },
   
-    async deleteServerChannelMember(req, res) {
+    async deleteServerUser(req, res) {
       let { userId } = req.params;
       let { serverId } = req.query
       const db = req.app.get('db');
-      let serverMembers = await db.delete_server_channel_member([
+      let serverUsers = await db.delete_server_user([
         +userId,
         +serverId,
       ]);
-      res.send(serverMembers);
+      res.send(serverUsers);
     },
 
-    async addServerChannelMember(req, res) {
+    async addServerUser(req, res) {
       let { userId, serverId } = req.body;
       const db = req.app.get('db');
-      let serverMembers = await db.add_server_channel_member([
+      let serverUsers = await db.add_server_user([
         userId,
         +serverId,
       ]);
      
-      res.send(serverMembers);
+      res.send(serverUsers);
     },
   }
