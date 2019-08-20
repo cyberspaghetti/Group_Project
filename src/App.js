ceb46b5ae0f900f48f3cdfc85f4c0757d1824f19
Tyler Hawkins <thawkinsusa>
2019-08-20 09:10:32 -0600

    put shit where it go as dawg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
 import routes from "./routes";
-import ServerRegister from "./components/registration/ServerRegistration"
-import ServersMap from './components/views/ServersMap'
+
 function App() {
   return <div>{routes}
-  <ServerRegister/>
-  <ServersMap/>
   </div>;
 }
 export default App;
