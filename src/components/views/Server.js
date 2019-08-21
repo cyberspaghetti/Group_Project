b83aa0632bc360d9a648df26cf2aac4333584b1a
Spence Robison <spence@MacBook-Pro-2.local>
2019-08-21 17:02:06 -0600

    styling homepage and serversmap

--- src/components/views/Server.js
+++ src/components/views/Server.js
@@ -2,28 +2,37 @@ import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import ListItem from "@material-ui/core/ListItem";
 import List from "@material-ui/core/List";
-import '../homepage/homepage.css'
+import "../homepage/homepage.css";
 
 //this is the component for a specified server it grabs props from the parent module that is uses it and displays the server information
+
+const listItemStyleServer = {
+  padding: "0px",
+  marginLeft: '7px'
+};
+
 export default class Server extends Component {
+  addDefaultSrc(ev) {
+    ev.target.src =
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvlhaYgj0EeSjYPBSHNY3xacbupTZ_EnCvlSWoyJB7jMa1wuhdeA";
+  }
+
   render() {
     return (
-
-        <ListItem button className='list-item-text'>
-          <div className="image-container">
-            <img
-              src=''
-              alt=""
-            />
-            <div
-              onClick={() =>
-                this.props.selectServer(this.props.server.server_id)
-              }
-            >
-              {this.props.server.server_name}
-            </div>
-          </div>
-        </ListItem>
+      <ListItem
+        button
+        style={listItemStyleServer}
+        onClick={() => this.props.selectServer(this.props.server.server_id)}
+        className="list-item-text"
+      >
+        <img
+          className="image-test"
+          src="this.props.server.server_image"
+          alt="server-pic"
+          onError={this.addDefaultSrc}
+        />
+        <div className='server-name-style'>{this.props.server.server_name}</div>
+      </ListItem>
     );
   }
 }
--- src/components/views/ServersMap.js
+++ src/components/views/ServersMap.js
@@ -3,9 +3,10 @@ import { connect } from "react-redux";
 import Server from "./Server";
 import List from "@material-ui/core/List";
 import { getAllServers } from "../../ducks/serverReducer";
+import "../homepage/homepage.css";
 
 // this component maps over ./server which are the servers that are apart of the specific user
-// the specific user id is grabbed from redux 
+// the specific user id is grabbed from redux
 class ServerMap extends Component {
   constructor() {
     super();
@@ -13,7 +14,7 @@ class ServerMap extends Component {
       servers: []
     };
   }
-// neeed to change to specific users to grab server
+  // neeed to change to specific users to grab server
   componentDidMount() {
     this.props.getAllServers().then(res => {
       this.setState({ servers: res.value });
@@ -26,11 +27,17 @@ class ServerMap extends Component {
     }
   }
 
+
   render() {
     let { servers } = this.state;
     return (
       <div className="hero-container">
-        <div onClick={() => this.props.selectServer(0)}>Home</div>
+        <img
+          src="yes"
+          alt=''
+          onClick={() => this.props.selectServer(0)}
+          className="news-icon"
+        />
         {servers.map(server => {
           return (
             <List>
