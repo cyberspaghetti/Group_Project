825fa6a2b25dc95c8164fbaff4f815d139da78f7
Jeordin Callister <callisterjeordin@gmail.com>
2019-08-21 00:39:54 -0600

    WORKING ROUTES

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,14 @@ import { Switch, Route } from "react-router-dom";
 import LandingPage from "./components/landingPage/LandingPage";
 import Homepage from "./components/homepage/Homepage";
 import ServerRegister from "./components/registration/ServerRegistration";
-import RoomsMap from "./components/JeordinTest/RoomsMap";
+import FakeHomepage from "./components/JeordinTest/FakeHomepage";
+// import RoomsMap from "./components/JeordinTest/RoomsMap";
 
 export default (
   <Switch>
     <Route exact path="/" component={LandingPage} />
     <Route exact path="/home" component={Homepage} />
-    <Route path="/registerServer" component={ServerRegister} />
-    <Route path="/jeordinTest" component={RoomsMap} />
+    <Route exact path="/registerServer" component={ServerRegister} />
+    <Route exact path="/jeordinTest" component={FakeHomepage} />
   </Switch>
 );
