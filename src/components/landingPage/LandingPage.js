3eac29cb98a2a4aa303d84e948fa365c24301dce
Tyler Hawkins <thawkinsusa>
2019-08-22 16:06:06 -0600

    the landing page has landed

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -4,8 +4,16 @@ import { getUser } from "../../ducks/userReducer";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
-class LandingPage extends Component {
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import "./landingpage.css"
 
+const buttonStyle = {
+  background: "#7e8699",
+  color: "#ffffff"
+};
+
+class LandingPage extends Component {
   componentDidMount() {
     if (!this.props.user.loggedIn) {
       this.props.getUser();
@@ -14,15 +22,24 @@ class LandingPage extends Component {
 
   userLogin = () => {
     window.location.href = "http://localhost:4000/api/login";
-  }
+  };
 
   render() {
     console.log(this.props);
     if (this.props.user.loggedIn) return <Redirect to="/home/0/0" />;
     return (
-      <div>
-        <button onClick={this.userLogin}>Login</button>
-        this is landing page
+      <div className="login-container">
+        <Box component="span" m={1} className="login-box">
+          <Button />
+          <div className="dude-container" />
+          <div className="button-container">
+            <Button
+              style={buttonStyle}
+              className="login-button"
+              onClick={this.userLogin}
+            >{`Login / Register`}</Button>
+          </div>
+        </Box>
       </div>
     );
   }
