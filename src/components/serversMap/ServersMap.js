bb636ea83235db7828373f43bcad42102b45b01c
Spence Robison <spence@MacBook-Pro-2.local>
2019-08-22 15:49:06 -0600

    please

--- src/components/serversMap/ServersMap.js
+++ src/components/serversMap/ServersMap.js
@@ -45,7 +45,6 @@ class ServerMap extends Component {
     return (
       <div className="hero-container">
         <ListItem button style={newsIconStyle}>
-<<<<<<< HEAD
           <div onClick={() => this.props.selectServer(0, 0)} className='menu-news'>
             <img
               src="https://images-workbench.99static.com/oe8mL8VlMNvQP6dLBIKXN812mMM=/0x0:1000x1000/fit-in/500x500/filters:fill(white,true)/99designs-contests-attachments/87/87933/attachment_87933369"
@@ -53,15 +52,6 @@ class ServerMap extends Component {
               className="news-icon"
             />
             <div>News</div>
-=======
-          <div className='menu-news' onClick={() => this.props.selectServer(0, 0)}>
-          <img
-            src="https://images-workbench.99static.com/oe8mL8VlMNvQP6dLBIKXN812mMM=/0x0:1000x1000/fit-in/500x500/filters:fill(white,true)/99designs-contests-attachments/87/87933/attachment_87933369"
-            alt=""
-            className="news-icon"
-          />
-          <div>News</div>
->>>>>>> 195cd38ad309f75188d9635cb8c64e802ca440bc
           </div>
         </ListItem>
         {servers.map(server => {
