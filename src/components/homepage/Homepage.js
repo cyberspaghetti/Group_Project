07d516aaf7979575784b2979ee42613ad08a76b0
Tyler Hawkins <thawkinsusa>
2019-08-23 14:34:09 -0600

    fixed toggle button and mapping for rooms

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -241,7 +241,7 @@ function Homepage(props) {
             onChange={e => changeNewRoom(e.target.value)}
 
             />
-            <Button onClick={() => props.createRoom(newRoom, props.match.params.selectedServer, props.match.params.selectRoomcreateRoom)}>SUBMIT</Button> 
+            <Button onClick={() =>  props.createRoom(newRoom, props.match.params.selectedServer, props.match.params.selectRoomcreateRoom).then(toggle())}>SUBMIT</Button> 
             <Button onClick={toggle} style={buttonStyle}>
               CANCEL
             </Button>
--- src/components/roomsMap/RoomsMap.js
+++ src/components/roomsMap/RoomsMap.js
@@ -5,17 +5,9 @@ import { createRoom, getRooms } from "../../ducks/roomReducer";
 //components
 import Room from "../room/Room";
 class RoomsMap extends Component {
-  constructor() {
-    super()
-    this.state = {
-      rooms: []
-    };
-  }
   
   componentDidMount = () => {
-    this.props.getRooms(this.props.selectedServer).then(res => {
-      this.setState({ rooms: res.value });
-    })
+    this.props.getRooms(this.props.selectedServer)
   }
   
   componentDidUpdate = prevProps => { 
@@ -27,18 +19,23 @@ class RoomsMap extends Component {
   };
   // <RoomsMap selectRoom={this.selectRoom} selectedServer={this.selectedServer}/>
   render() {
-    let { rooms } = this.state;
+    console.log('props in rooms',this.props)
+    let rooms = 'loading'
+    if(this.props.rooms.rooms)
     return (
       <section className="full-room-holder">
-        {rooms.map(obj => {
-          return <Room obj={obj} key={obj.socket_room_id} selectedServer={this.props.selectedServer} selectRoom={this.props.selectRoom}/>;
+        {rooms = this.props.rooms.rooms.map(rooms => {
+          return <Room rooms={rooms} key={rooms.socket_room_id} selectedServer={this.props.selectedServer} selectRoom={this.props.selectRoom}/>;
         })}
       </section>
     );
+    return(
+      <div>{rooms}</div>
+    )
   }
 }
 function mapStateToProps(state) {
-  return {};
+  return {rooms: state.rooms};
 }
 export default connect(
   mapStateToProps,
