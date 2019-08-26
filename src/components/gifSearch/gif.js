dbd53ca4c70509bd0f1f9640e70b516a409fef76
Tyler Hawkins <thawkinsusa@gmail.com>
2019-08-25 21:07:58 -0600

    fixed search

--- src/components/gifSearch/gif.js
+++ src/components/gifSearch/gif.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './gif.css'
 //this is the component for the user it grabs props from the parent module that uses it and displays the specific user information 
 export default function Gif(props){ 
-console.log('props', props);
         return (
             
-            <div className='image-container'>
-                <img src={props.gifs.images.preview_gif.url} />
+            <div className='gif-container'>
+                <img src={props.gifs.images.preview_gif.url} className='gif-img'/>
             </div>
         )
     }
--- src/components/gifSearch/gifSearch.js
+++ src/components/gifSearch/gifSearch.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Gif from './gif'
+import './gif.css'
 
 
 
@@ -10,47 +11,49 @@ export default class GifSearch extends Component {
         this.state = {
             search: '',
             gifs: {
-                data: {data: []}
+                data: { data: [] }
             }
         }
     }
 
-    componentDidUpdate = (prevProps, prevState) => { 
-        if (prevState.gifs != this.state.gifs )
-        this.render() 
-        }
-
-    search = () =>{
-        let {search} = this.state
-        axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=4YfuzIBXGL7xgZfoNJuNN2NVfZPoRjFD&limit=5`)
-        .then(res => {
-            this.setState({ gifs: res });
-            console.log(' gifs from gif', res)
-          }) 
+    componentDidUpdate = (prevProps, prevState) => {
+        if (prevState.gifs != this.state.gifs)
+            this.render()
     }
-    handleServerChange = e => {
+
+    search = () => {
+        let { search } = this.state
+        axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=4YfuzIBXGL7xgZfoNJuNN2NVfZPoRjFD&limit=10`)
+            .then(res => {
+                this.setState({ gifs: res });
+                console.log(' gifs from gif', res)
+            })
+    }
+    handleChange = e => {
         const { name, value } = e.target;
         this.setState({ [name]: value });
-      };
-    
+    };
+
     render() {
         let { search, gifs } = this.state
-    
-        console.log('gif props',this.state)
+
+        console.log('gif props', this.state)
         return (
             <div className='server-registration-container'>
-            <div className='border-of-component'>
-                <title>Register server</title>
-                <h1>Search Term</h1>
-                <input onChange={this.handleServerChange} value={search} name='search'/>
-                <button onClick={this.search}>search</button>
-            </div>
-            <div className='users-container'>{gifs.data.data.map(gifs => {
-                return (
-                    <Gif gifs={gifs} key={gifs.id} className='user-container' />
-                )
-            })}
-            </div>
+                <div className='border-of-component'>
+                    <h1>GIF
+                    <input onChange={this.handleChange} value={search} name='search' />
+                        <button onClick={this.search}>search</button>
+                    </h1>
+                <div className='test'>
+                    <div className='gif-container'>{gifs.data.data.map(gifs => {
+                        return (
+                            <Gif gifs={gifs} key={gifs.id} className='gif-img' />
+                        )
+                    })}
+                    </div>
+                </div>
+                </div>
             </div>
         )
     }
